11646cfee7af8fc63eff4930d14f4f63a1822c49
jcarlosmolina <jcarlosmolina@gmail.com>
2017-10-14 11:37:35 +0200

    Take code to final stage of local-variables-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,17 @@ export class AppComponent  {
     this.hideEmail = !this.hideEmail;
   }
 
+  private changeMail(newMail:string) {
+    this.currentUser.email = newMail;
+  }
+
+  private keyPress(event:KeyboardEvent) {
+    if(event.keyCode == 13) {
+      var inputElement = <HTMLInputElement>event.target;
+      this.changeMail(inputElement.value);
+    }
+  }
+
 }
 
 interface User {
