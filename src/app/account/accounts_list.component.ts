1da2b2fb8423c399178bd17195c5cbbf816facf7
jcarlosmolina <jcarlosmolina@gmail.com>
2017-11-06 10:07:10 +0100

    Take code to final stage of decoratorINPUT

--- src/app/account/accounts_list.component.ts
+++ src/app/account/accounts_list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Account} from './account.model';
 
 @Component({
@@ -7,24 +7,12 @@ import {Account} from './account.model';
   styleUrls: ['app/account/accounts_list.component.css']
 })
 export class AccountsList {
-  private _selected:Array<boolean> = [false, false];
-  private _accounts:Array<Account> = [
-    {
-      id: 1,
-      title: "Bank xyz",
-      description: "This is my main bank account",
-      balance: 501.2
-    },
-    new Account(2, "Bank Asd", "My secret account", 1024.10)
-  ];
+
+  @Input('accounts')
+  private _accounts:Array<Account>;
 
   private removeAcc(index:number) {
     this._accounts.splice(index, 1);
-    this._selected.splice(index, 1);
   }
 
-
-  private select(index:number) {
-    this._selected[index] = !this._selected[index];
-  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,22 @@ import { AccountsList } from './account/accounts_list.component';
 })
 export class AppComponent  {
 
+  private _accounts:Array<Account> = [
+    {
+      id: 1,
+      title: "Bank xyz",
+      description: "This is my main bank account",
+      balance: 501.2
+    },
+    {
+      id: 2,
+      title: "Bank asd",
+      description: "My secret bank account",
+      balance: 1024.10
+    }
+    // new Account(2, "Bank Asd", "My secret account", 1024.10)
+  ];
+
   private _nextId = 3;
 
   private createAcc(titleEl:any, descEl:any, balEl:any){
