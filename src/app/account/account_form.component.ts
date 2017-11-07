427c0ed86c94abc6e98966b86d7eea6262ed9a8c
jcarlosmolina <jcarlosmolina@gmail.com>
2017-11-07 06:23:20 +0100

    Stage 1

---
+++ src/app/account/account_form.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Account } from './account.model';
+
+@Component({
+  selector: 'account-form',
+  templateUrl: 'app/account/account_form.component.html',
+  styleUrls: ['app/account/account_form.component.css']
+})
+export class AccountForm {
+
+}
